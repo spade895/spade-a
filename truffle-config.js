51305695d1fad3ef547e5cd5fd5b72e64fa774c2
spade895 <57581903+spade895@users.noreply.github.com>
2019-11-10 15:24:21 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,22 +6,7 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-     ropsten: {
-      provider: function() {
-       return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/b9f457e843804c40b9bddc1db7903cc7")
-      },
-      network_id: 3
-    },
-    mainnet: {
-      gas: 7500000,
-      gasPrice: 6000000000,
-      provider: function() {
-          return new HDWalletProvider(mnemonic,
-              "https://mainnet.infura.io/v3/b9f457e843804c40b9bddc1db7903cc7")
-      },
-      network_id: 1
-
-  },
+   
     development: {
       host: "127.0.0.1",
       port: 8545,
