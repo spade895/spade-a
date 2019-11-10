eaba88abf472c3589e38baa0a0e8ef2b00b06162
spade895 <57581903+spade895@users.noreply.github.com>
2019-11-10 15:24:52 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 // 提供助记词（mnemonic）来生成你的账户
-const  mnemonic = "fluid cruise hire farm relax tone bless joke dilemma cushion senior detail";
+const  mnemonic = "";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
